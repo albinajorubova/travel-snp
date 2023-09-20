4ea0877417b4944df4efba22f9cd9d7db0386541
Jorubova Albina <deletawer@gmail.com>
2023-09-21 00:05:47 +0300

    final

---
+++ assets/js/js.js
@@ -0,0 +1,35 @@
+const scrollHead = document.getElementById("scroll1");
+const height = 450;
+window.addEventListener("scroll", (ev) => {
+  console.log("hi");
+  if (scrollY > height) {
+    scrollHead.style.display = "block";
+  } else {
+    scrollHead.style.display = "none";
+  }
+});
+
+const tabsBtn = document.querySelectorAll(".main__link");
+const tabsItems = document.querySelectorAll(".navlink-item");
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener("click", function () {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+
+    if (!currentBtn.classList.contains("active")) {
+      tabsBtn.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabsItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
